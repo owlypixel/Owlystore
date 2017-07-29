e6fc679f9e5fbbdce11082c5ed3d88ad066041cc
Owlypixel <owlypixel@gmail.com>
2017-07-29 23:13:40 +0300

    file upload

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,21 @@
 
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter: function(req, file, next) {
+		const isPhoto = file.mimetype.startsWith('image/');
+		if(isPhoto){
+			next(null, true);
+		} else {
+			next({message: 'That file type is not allowed!'})
+		}
+	} 
+}
 
 exports.homePage = (req, res) => {
 	console.log(req.name);
@@ -9,6 +24,23 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store' });
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+	if(!req.file){
+		next();
+		return;
+	} 
+	// console.log(req.file);
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+	console.log(req.body.photo);
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+	next();
 }
 
 exports.createStore = async (req, res) => {
