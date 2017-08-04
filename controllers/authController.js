192f632331ce5610946fc056e621bf0a302f02a1
Owlypixel <owlypixel@gmail.com>
2017-08-04 23:57:42 +0300

    account

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,4 +4,19 @@ exports.login = passport.authenticate('local', {
 	failureFlash: 'Failed login!',
 	successRedirect: '/',
 	successFlash: 'You are now logged in!'
-})
+});
+
+exports.logout = (req, res) => {
+	req.logout();
+	req.flash('success', 'You are now logged out!');
+	res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+	if(req.isAuthenticated()){
+		next();
+		return;
+	}
+	req.flash('error', 'Oopps! You must be logged in!');
+	res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const {catchErrors} = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.post('/add',
 	storeController.upload,
 	catchErrors(storeController.resize),
@@ -26,11 +26,16 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags/', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 router.post('/register',
 	userController.validateRegister,
 	userController.register,
 	authController.login
 );	
+
+router.get('/logout', authController.logout);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
